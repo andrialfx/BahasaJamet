fc42527e16c044d0102537daac6c3eea7ff67635
zhafranammar <mzhafranammar@gmail.com>
2022-04-20 22:39:56 +0700

    menambahkan program utama untuk membaca bahasa melalui command pada terminal

---
+++ main.py
@@ -0,0 +1,19 @@
+import jametLang_interpreter
+import jametLang_lexer
+import jametLang_parser
+
+from sys import *
+
+#MASUKAN LANGSUNG
+if __name__ == '__main__':
+    lexer = jametLang_lexer.leksikal()
+    parser = jametLang_parser.sintaksis()
+    env = {}
+    while True:
+        try:
+            text = input('j4m3t > ')
+        except EOFError:
+            break
+        if text:
+            tree = parser.parse(lexer.tokenize(text))
+            jametLang_interpreter.BasicExecute(tree, env)
